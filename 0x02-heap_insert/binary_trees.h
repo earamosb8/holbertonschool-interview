0e4895a144df736d2f8ecad0b52c023837a5dea3
earamosb8 <earamosb8@gmail.com>
2021-02-11 23:11:12 -0500

    add betty in header file

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -30,4 +30,4 @@ heap_t *exchange_order(heap_t *son);
 size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
-#endif /* _BINARY_TREES_H_ */
+#endif/* _BINARY_TREES_H_ */
