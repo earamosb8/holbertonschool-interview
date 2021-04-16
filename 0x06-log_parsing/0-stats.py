1bbc268ddc8eb5d923239e5cb93f61c115bf67c8
earamosb8 <earamosb8@gmail.com>
2021-04-16 16:03:23 -0500

    fix stats

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics"""
+""" Module that reads stdin line """
 
 import sys
 
+status_code = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+output = 0
 
-i = 0
-sum_file_size = 0
-status_code = {'200': 0,
-               '301': 0,
-               '400': 0,
-               '401': 0,
-               '403': 0,
-               '404': 0,
-               '405': 0,
-               '500': 0}
 
-try:
-    for line in sys.stdin:
-        args = line.split(' ')
-        if len(args) > 2:
-            status_line = args[-2]
-            file_size = args[-1]
-            if status_line in status_code:
-                status_code[status_line] += 1
-            sum_file_size += int(file_size)
-            i += 1
-            if i == 10:
-                print('File size: {:d}'.format(sum_file_size))
-                sorted_keys = sorted(status_code.keys())
-                for key in sorted_keys:
-                    value = status_code[key]
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-                i = 0
-except Exception:
-    pass
-finally:
-    print('File size: {:d}'.format(sum_file_size))
-    sorted_keys = sorted(status_code.keys())
-    for key in sorted_keys:
-        value = status_code[key]
-        if value != 0:
-            print('{}: {}'.format(key, value))
+def main():
+    """ Method that prints stdin line by line """
+    print("File size: {}".format(output))
+    for status in sorted(status_code.keys()):
+        if status_code[status]:
+            print("{}: {}".format(status, status_code[status]))
+
+
+if __name__ == '__main__':
+    count = 0
+    try:
+        for line in sys.stdin:
+            try:
+                words = line.split()
+                output += int(words[-1])
+                if words[-2] in status_code:
+                    status_code[words[-2]] += 1
+            except:
+                pass
+            if count == 9:
+                main()
+                count = -1
+            count += 1
+    except KeyboardInterrupt:
+        main()
+        raise
+    main()
