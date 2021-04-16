5e4cd8f71204c0f400e397d6a58f5b49d18d1ced
earamosb8 <earamosb8@gmail.com>
2021-04-16 16:11:26 -0500

    fix stats

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python3
-"""
-    Reads stdin line by line and computes metrics:
-    For every 10 lines:
-        - print the status number with the number of times it
-        appears
-        - print the sum of the file sizes
-"""
+"""script that reads stdin line by line and computes metrics"""
+import sys
+
+
+def formatPrint(size, myDict):
+    """format de dictionary"""
+    print("File size: {}".format(size))
+    for key, value in sorted(statusDict.items()):
+        if (value != 0):
+            print("{}: {}".format(key, value))
+
+
 if __name__ == "__main__":
-    import sys
-    import signal
-
-    c = fileSize = 0
-    statCount = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                 "404": 0, "405": 0, "500": 0}
-
-    def handleTen(statCount, fileSize):
-        print("File size: {}".format(fileSize))
-        for key in sorted(statCount.keys()):
-            if statCount[key] == 0:
-                continue
-            print("{}: {}".format(key, statCount[key]))
-
+    """init code to print the parsed data"""
+    statusList = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    statusDict = {}
+    size = 0
+    i = 0
     try:
         for line in sys.stdin:
-            c += 1
-            split = line.split(" ")
-            try:
-                status = split[-2]
-                fileSize += int(split[-1])
-
-                if status in statCount:
-                    statCount[status] += 1
-            except Exception:
-                pass
-
-            if c % 10 == 0:
-                handleTen(statCount, fileSize)
-
-        else:
-            handleTen(statCount, fileSize)
-
-    except (KeyboardInterrupt, SystemExit):
-        handleTen(statCount, fileSize)
+            i += 1
+            if (len(line.split()) < 2):
+                continue
+            lineList = line.split()
+            statusCode = lineList[-2]
+            size = size + int(lineList[-1])
+            if statusCode in statusList:
+                try:
+                    statusDict[statusCode] += 1
+                except KeyError:
+                    statusDict[statusCode] = 1
+            if i % 10 == 0:
+                formatPrint(size, statusDict)
+        formatPrint(size, statusDict)
+    except KeyboardInterrupt:
+        formatPrint(size, statusDict)
         raise
