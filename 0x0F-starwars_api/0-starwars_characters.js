d3db71b7a55550547b06e6c924268a16029ebaec
earamosb8 <earamosb8@gmail.com>
2021-05-21 10:26:29 -0500

    add starwars

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+let request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+  if (!err) {
+    let characters = JSON.parse(body).characters;
+    recursionRequestPrint(characters, 0);
+  }
+});
+
+function recursionRequestPrint (url, index) {
+  request(url[index], (err, res, body) => {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+    }
+});
