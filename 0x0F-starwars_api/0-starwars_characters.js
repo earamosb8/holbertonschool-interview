dc438f05da530a5bb6ca87cdbb71df8d787da226
earamosb8 <earamosb8@gmail.com>
2021-05-21 10:29:27 -0500

    add starwars

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,16 +1,32 @@
 #!/usr/bin/node
-let request = require('request');
-request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
-  if (!err) {
-    let characters = JSON.parse(body).characters;
-    recursionRequestPrint(characters, 0);
-  }
-});
 
-function recursionRequestPrint (url, index) {
-  request(url[index], (err, res, body) => {
-    if (!err) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+let request = require('request');
+
+async function sw (id) {
+  let url = `https://swapi-api.hbtn.io/api/films/${id}`;
+
+  request(url, async function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      for (let ch of JSON.parse(body).characters) {
+        let ret = () => {
+          return new Promise((resolve, reject) => {
+            request(ch, function (err, response, body) {
+              if (err) {
+                console.log(err);
+              } else {
+                resolve(JSON.parse(body).name);
+              }
+            });
+          });
+        };
+        console.log(await ret());
+      }
     }
-});
+  });
+}
+
+if (process.argv.length === 3) {
+  sw(process.argv[2]);
+}
