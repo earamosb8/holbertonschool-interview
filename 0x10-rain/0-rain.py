122a8018ae867acd3c1428b5e7e4eac1b4052441
earamosb8 <earamosb8@gmail.com>
2021-05-27 15:19:39 -0500

    add task 0 rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""0.Rain
+"""
+
+
+def rain(walls):
+    """Prototype: def rain(walls)
+       walls is a list of non-negative integers.
+       Return: Integer indicating total amount of rainwater retained.
+       Assume that the ends of the list (before index 0 and after index walls[-1]) are not walls, meaning they will not retain water.
+       If the list is empty return 0.
+    """
+    i = maxleft = maxright = water = 0
+    j = len(walls) - 1
+
+    while i < j:
+        if walls[i] < walls[j]:
+            if walls[i] < maxleft:
+                water += maxleft - walls[i]
+            maxleft = max(maxleft, walls[i])
+            i += 1
+        else:
+            if walls[j] < maxright:
+                water += maxright - walls[j]
+            maxright = max(maxright, walls[j])
+            j -= 1
+
+    return water
