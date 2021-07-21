695a91b1ccf22fa1a0bb3b323874dccc521908c1
Eduardo Ramos <60366913+earamosb8@users.noreply.github.com>
2021-07-21 17:37:41 -0500

    Update sort.h

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -1,10 +1,11 @@
-#ifndef MERGE_S
-#define MERGE_S
-
+#ifndef SORT_H
+#define SORT_H
 #include <stdio.h>
 #include <stdlib.h>
 
-void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge_sort_holder(int *array, size_t size, int *holder);
+void merge(int *holder, int *array, int mid, size_t size);
 
 #endif
